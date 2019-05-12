a95cf98fa2aa698485e8fb277dd7944517db208f
Subin Siby <subins2000@gmail.com>
2019-05-12 22:49:47 +0530

    Initial commit ==============
    Basic view

---
+++ app/src/main/java/subins2000/manglish/MainActivity.kt
@@ -0,0 +1,14 @@
+package subins2000.manglish
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
