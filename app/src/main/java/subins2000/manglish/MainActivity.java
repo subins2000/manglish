2190299017aa661e583e372d6f1aff8e7d4c091e
Alex Churkin <churkindev@gmail.com>
2019-06-29 22:07:48 +0300

    Navigation fixed; home layout improved; unnecessary files excluded from tracking

--- app/src/main/java/subins2000/manglish/MainActivity.java
+++ app/src/main/java/subins2000/manglish/MainActivity.java
@@ -1,75 +1,96 @@
 /**
  * Manglish, malayalam to manglish converter
  * Copyright (C) 2019 Subin Siby
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package subins2000.manglish;
 
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+
 import com.google.android.material.navigation.NavigationView;
 
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, HomeFragment.OnFragmentInteractionListener, AboutFragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        HomeFragment.OnFragmentInteractionListener, AboutFragment.OnFragmentInteractionListener {
 
+    private static final String NAV_ID = "NavId";
+
+    DrawerLayout drawer;
     NavigationView navigationView;
 
-    Fragment homeFragment = new HomeFragment();
-    Fragment aboutFragment = new AboutFragment();
+    SharedPreferences prefs;
+
+    int activeNavId = R.id.nav_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
+        drawer.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if(slideOffset > 0) tryHideKeyboard();
+            }
+        });
         toggle.syncState();
 
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         navigationView.getMenu().getItem(0).setChecked(true);
 
-        // Add fragments
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.frame, homeFragment);
-        transaction.add(R.id.frame, aboutFragment);
-        transaction.commit();
+        if (savedInstanceState == null) {
+            setFragment(new HomeFragment());
+        } else {
+            activeNavId = savedInstanceState.getInt(NAV_ID);
+            navigationView.getMenu().findItem(activeNavId).setChecked(true);
+        }
+    }
 
-        showFragment(homeFragment);
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ID, activeNavId);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -91,53 +112,54 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_about) {
-            showFragment(aboutFragment);
-            navigationView.getMenu().getItem(1).setChecked(true);
+        if (id == R.id.action_about && activeNavId != R.id.nav_about) {
+            setFragment(new AboutFragment());
+            Menu menu = navigationView.getMenu();
+            menu.findItem(R.id.nav_about).setChecked(true);
+            menu.performIdentifierAction(R.id.nav_about, 0);
+            activeNavId = R.id.nav_about;
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void showFragment(Fragment frag) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.hide(homeFragment);
-        transaction.hide(aboutFragment);
-        transaction.show(frag);
-        transaction.commit();
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        Fragment frag = null; // create a Fragment Object
-
-        navigationView.setCheckedItem(id);
-
-        if (id == R.id.nav_home) {
-            frag = homeFragment;
-        } else if (id == R.id.nav_about) {
-            frag = aboutFragment;
-        }
-
-        if (frag != null) {
-            showFragment(frag);
-
-            // close the all open Drawer Views
-            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (activeNavId != id) {
+            activeNavId = id;
+            switch (id) {
+                case R.id.nav_home:
+                    setFragment(new HomeFragment());
+                    break;
+                case R.id.nav_about:
+                    setFragment(new AboutFragment());
+                    break;
+            }
             drawer.closeDrawer(GravityCompat.START);
-
             return true;
         }
         return false;
     }
 
     @Override
-    public void onFragmentInteraction(Uri uri){
+    public void onFragmentInteraction(Uri uri) {
         //you can leave it empty
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame, fragment)
+                .commit();
+    }
+
+    private void tryHideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        View curFocus = getCurrentFocus();
+        if (curFocus != null) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
     }
 }
